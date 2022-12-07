b3abfbaff740753c3fc0125a4cc1aa81bf6f86df
cokie1479 <70733789+cokie1479@users.noreply.github.com>
2022-12-06 20:11:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ git clone "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git" stable
 wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -P stable-diffusion-webui/models/Stable-diffusion #stable-diffusion-v-1-4 Model
 wget https://huggingface.co/hakurei/waifu-diffusion-v1-3/blob/main/wd-v1-3-float16.ckpt -P stable-diffusion-webui/models/Stable-diffusion #waifu-diffusion-v1-3 Model
 wget https://huggingface.co/prompthero/openjourney/resolve/main/mdjrny-v4.ckpt -P stable-diffusion-webui/models/Stable-diffusion #OpenJourney/MidJourney V4 Model
-python3 /stable-diffusion-webui/launch.py
+python3 stable-diffusion-webui/launch.py
