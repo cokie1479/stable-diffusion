262e92b0688562f969e8c4838ff3b6ee0531087b
cokie1479 <70733789+cokie1479@users.noreply.github.com>
2022-12-08 08:44:28 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git #The web ui
 git clone "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git" stable-diffusion-webui/extensions/tag-autocomplete #Tag auto-complete addon
-
+git clone "https://huggingface.co/args/models" stable-diffusion-webui/models
 
 python3 stable-diffusion-webui/launch.py
